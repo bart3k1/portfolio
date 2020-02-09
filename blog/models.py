cdcc86373edb2c1be0aa24149c12ec65f41dc46b
bart3k1 <bart3k1@gmail.com>
2020-02-09 20:46:23 +0100

    homepage template added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Blog(models.Model):
+    image = models.ImageField(upload_to="images/")
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    pub_date = models.DateTimeField(default=timezone.now)
